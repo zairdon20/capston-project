a5cc6b4680a11e65af42c026bcddfe19435947de
Fatima Ezzahra <felemenoun@gmail.com>
2021-12-24 21:56:11 +0100

    fix about page

--- about.js
+++ about.js
@@ -1,4 +1,4 @@
-/*const grab = (e) => document.querySelector(`.${e}`);
+/* const grab = (e) => document.querySelector(`.${e}`);
 
 // ABOUT PAGE MENU
 
@@ -8,27 +8,32 @@ grab('fa-bars').addEventListener('click', () => {
 
 grab('close').addEventListener('click', () => {
   grab('menu-lis', false, false).style.display = 'none';
-});*/
-  
-  /* MENU */
-  
-  const btn = document.querySelector('.hamburger');
-  
-  const icon = document.querySelector('.icon');
-  
-  const navElements = document.querySelector('.nav-element');
-  
-  btn.addEventListener('click', displayMenu);
-  
-  function displayMenu () {
-      if(icon.classList.contains('menu')) {
-          icon.classList.remove('menu');
-          icon.classList.add('close');
-          navElements.style.display = 'block';
-      }
-      else if(icon.classList.contains('close')) {
-          icon.classList.remove('close');
-          icon.classList.add('menu');
-          navElements.style.display = 'none';
-      }
-  }
+}); */
+
+/* MENU */
+
+const navElements = document.querySelector('.nav-element');
+
+// function displayMenu () {
+//     if(icon.classList.contains('menu')) {
+//         icon.classList.remove('menu');
+//         icon.classList.add('close');
+//         navElements.style.display = 'block';
+//     }
+//     else if(icon.classList.contains('close')) {
+//         icon.classList.remove('close');
+//         icon.classList.add('menu');
+//         navElements.style.display = 'none';
+//     }
+// }
+
+function openMenu() {
+  navElements.style.display = 'block';
+}
+
+function closeMenu() {
+  navElements.style.display = 'none';
+}
+
+openMenu();
+closeMenu();
